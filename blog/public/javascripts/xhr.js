9c15d12d8d45de6230e042a3483c8850a9712a24
daddyderek <daddyderekahn@gmail.com>
2014-02-19 11:54:35 -1000

    got toggle nav to work

--- blog/public/javascripts/xhr.js
+++ blog/public/javascripts/xhr.js
@@ -13,4 +13,10 @@ $(document).ready(function() {
 			} // ends success:
 		}); // ends $.ajax
 	}); // ends $(form).on
+
+	// toggle the blue side nav-bar
+	$('[data-toggle=offcanvas]').click(function () {
+		console.log("i'm in data-toggle");
+    $('.row-offcanvas').toggleClass('active');
+  });// ends $('[data-toggle=offcanvas]')
 }); // ends $doc.ready
