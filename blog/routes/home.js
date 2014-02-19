8db064aefcf3f8948a9a3ce7d3d8600ec390f6b4
daddyderek <daddyderekahn@gmail.com>
2014-02-18 17:35:38 -1000

    new post and home working

--- blog/routes/home.js
+++ blog/routes/home.js
@@ -3,12 +3,29 @@ var app = module.exports = express();
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/blogIt');
 
-function Resource(req, res) {
-
+// app.get("/home")
+function homePage(req, res) {
 	res.render('index');
-}// ends homePage
+} // ends homePage
 
- // ROUTE HANDLERS 
+
+// app.get("/home/newPost")
+function newPost(req, res) {
+	// console.log("i'm working in new post");
+	res.render('newPost');
+}// ends newPost
+
+
+
+
+/*		ROUTE HANDLERS		*/
+
+// Calls the home page
 app.get("/home", function(req, res) {
-	Resource(req, res);
+	homePage(req, res);
+});
+
+// Calls the new post page
+app.get("/home/newPost", function(req, res) {
+	newPost(req, res);
 });
