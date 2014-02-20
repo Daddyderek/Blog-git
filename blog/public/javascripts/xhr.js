48f4e4bda8eb0fdc94587673decc3289a08ac4dd
daddyderek <daddyderekahn@gmail.com>
2014-02-20 09:35:45 -1000

    newPost working, and home is working

--- blog/public/javascripts/xhr.js
+++ blog/public/javascripts/xhr.js
@@ -1,22 +1,38 @@
 $(document).ready(function() {
 	console.log("JavaScript is working!");
 
+	// delete blog post
+	$('button.delete').on('click', function(e) {
+		var button = $(this);
+		e.preventDefault();
+		console.log("in delete");
+		$.ajax(button.attr('href'), {
+			method: 'DELETE',
+			success: function(result) {
+				console.log("inside delete meow");
+				button.fadeOut('fast');
+			}// ends success
+		});// ends $.ajax	
+	});// ends #delete
+
 	// create new blog post
-	$('form').on('submit', function(event) {
+	$('form#blogForm').on('submit', function(event) {
 		event.preventDefault();
 		$.ajax('/home/newPost', {
-			type: 'POST',
-			data: $('form').serialize(),
+			method: 'POST',
+			data: $('form#blogForm').serialize(),
 			success: function(result) {
-				$('form').val("").remove();
+				$('form#blogForm').html("");
 				$('#result').html('Your Post was successful');
 			} // ends success:
 		}); // ends $.ajax
-	}); // ends $(form).on
+	}); // ends $(form#blog).on
+
 
 	// toggle the blue side nav-bar
 	$('[data-toggle=offcanvas]').click(function () {
 		console.log("i'm in data-toggle");
     $('.row-offcanvas').toggleClass('active');
   });// ends $('[data-toggle=offcanvas]')
+
 }); // ends $doc.ready
--- blog/routes/home.js
+++ blog/routes/home.js
@@ -10,9 +10,10 @@ var schema = new mongoose.Schema({title: String, content: String, date: {type: D
 var post = mongoose.model('post', schema);// ends post
 
 
-// app.get("/home")
+// // app.get("/home")
 function homePage(req, res) {
-	post.find(function(err, blogPost) {
+
+	post.find().limit(6).exec(function(err, blogPost) {
 		if(err) {
 			console.log("error meow");
 		}
@@ -22,10 +23,31 @@ function homePage(req, res) {
 	});// ends res.render
 	});// ends post.find
 } // ends homePage
+
+// // app.get("/home")
+// function homePage(req, res) {
+// 	post.find(function(err, blogPost) {
+// 		if(err) {
+// 			console.log("error meow");
+// 		}
+// 		console.log(blogPost);
+// 		res.render('index', {
+// 		posts: blogPost
+// 	});// ends res.render
+// 	});// ends post.find
+// } // ends homePage
+       
        
 
 // app.get("/home/newPost")
 function newPost(req, res) {
+
+	res.render('newPost');
+}// ends newPost
+
+
+// app.post('/home/newpost')
+function createPost(req, res) {
 	var postTitle = req.body.title;
 	var postContent = req.body.content;
 	var postDate = req.body.date;
@@ -40,10 +62,38 @@ function newPost(req, res) {
 			res.send("Server Error");
 		}
 	});// ends myPost.save
+	res.render("newPost");
+}// ends createPost
 
-	res.render('newPost');
-}// ends newPost
 
+
+// app.delete("/home")
+function deletePost(req, res) {
+	var idNum = req.param('id');
+	post.findOneAndRemove({
+		_id: idNum
+	}, function(err) {
+		if(err) {
+			console.log("WTF?");
+		}
+		console.log("deleted");
+		res.send('success');
+	});
+}// ends deletePost
+
+
+
+// app.get("/home/oldPosts")
+function oldPosts(req, res) {
+	post.find(function(err, myOldPosts) {
+		if(err) {
+			console.log("error in oldPosts serverside");
+		}
+		res.render('oldPosts', {
+			posts: myOldPosts
+		});
+	});// ends post.find
+}// ends oldPosts
 
 
 
@@ -53,7 +103,19 @@ function newPost(req, res) {
 app.get("/home", function(req, res) {
 	homePage(req, res);
 });
-// Calls the new post page to POST
-app.post("/home/newpost", function(req, res) {
+// Calls the new post page
+app.get("/home/newPost", function(req, res) {
 	newPost(req, res);
 });
+// Calls the new post page to POST
+app.post("/home/newpost", function(req, res) {
+	createPost(req, res);
+});
+// On the home page it deletes the current post
+app.delete('/home/:id', function(req, res) {
+	deletePost(req, res);
+});
+// Calls the old posts
+app.get("/home/oldPosts", function(req, res) {
+	oldPosts(req, res);
+});// ends app.get("/home/oldPosts")
